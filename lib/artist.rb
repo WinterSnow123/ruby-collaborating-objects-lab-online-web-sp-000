8d31273c27279d8fb190f735e6abf8f33698eb05
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-29 21:08:30 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -32,7 +32,7 @@ def self.find_or_create_by_name(name)
 end
 
 def print_songs
-  Song.all.select{|song| song.artist == self ? puts song.name}
+  Song.all.each{|song| song.artist == self ? puts song.name}
 end
 
 end
